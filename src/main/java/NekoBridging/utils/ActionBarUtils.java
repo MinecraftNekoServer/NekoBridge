ecbb0f3da4c4707877df648644ebfdc6b296fe1e
Nyanyagulugulu <qianxun114514@gmail.com>
2025-12-12 17:27:46 +0800

    修复错误

--- src/main/java/NekoBridging/utils/ActionBarUtils.java
+++ src/main/java/NekoBridging/utils/ActionBarUtils.java
@@ -53,7 +53,38 @@ public class ActionBarUtils {
                 ppoc = classPacketChat.getConstructor(new Class<?>[]{classIChatComponent, byte.class}).newInstance(cbc, (byte) 2);
             } else {
                 Object o = classChatComponentText.getConstructor(new Class<?>[]{String.class}).newInstance(message);
-                ppoc = classPacketChat.getConstructor(new Class<?>[]{classIChatComponent, byte.class}).newInstance(o, (byte) 2);
+                // 尝试不同的构造函数，因为1.12.2可能有不同的参数
+                try {
+                    ppoc = classPacketChat.getConstructor(new Class<?>[]{classIChatComponent, byte.class}).newInstance(o, (byte) 2);
+                } catch (NoSuchMethodException e) {
+                    // 如果byte参数的构造函数不存在，则尝试只使用IChatBaseComponent的构造函数
+                    ppoc = classPacketChat.getConstructor(new Class<?>[]{classIChatComponent}).newInstance(o);
+                    // 然后通过反射设置消息类型为2（Action Bar）
+                    // 注意：在1.12.2中，字段类型可能是ChatMessageType而不是byte
+                    try {
+                        Field typeField = ppoc.getClass().getDeclaredField("b");
+                        typeField.setAccessible(true);
+                        // 尝试获取ChatMessageType枚举
+                        Class<?> chatMsgTypeClass = Class.forName("net.minecraft.server." + nmsver + ".ChatMessageType");
+                        Object[] chatMsgTypes = chatMsgTypeClass.getEnumConstants();
+                        if (chatMsgTypes.length > 2) {
+                            typeField.set(ppoc, chatMsgTypes[2]); // 2对应Action Bar
+                        } else {
+                            // 如果枚举常量不够，使用第一个
+                            typeField.set(ppoc, chatMsgTypes[0]);
+                        }
+                    } catch (Exception ex) {
+                        // 如果无法设置类型，使用旧方法尝试
+                        try {
+                            Field typeField = ppoc.getClass().getDeclaredField("b");
+                            typeField.setAccessible(true);
+                            typeField.set(ppoc, (byte) 2);
+                        } catch (Exception ex2) {
+                            // 如果还是失败，则忽略，消息仍会发送但可能不是action bar
+                            System.out.println("Warning: Could not set chat message type");
+                        }
+                    }
+                }
             }
             Object h = methodGetHandle.invoke(p);
             Object pc = fieldConnection.get(h);
--- src/main/java/NekoBridging/utils/NoAIUtils.java
+++ src/main/java/NekoBridging/utils/NoAIUtils.java
@@ -1,63 +1,83 @@
 package NekoBridging.utils;
 
 import org.bukkit.entity.Entity;
-
 import java.lang.reflect.Method;
 
 public class NoAIUtils {
-    private static boolean works;
-    private static Class<?> clsCraftEntity = null;
-    private static Method mhdGetHandle = null;
-    private static Method mhdGetNBTTag = null;
-    private static Class<?> clsNBTTagCompound = null;
-    private static Method mhdSetInt = null;
-    private static Method mhdReadTag = null;
-    private static Method mhdSetTag = null;
+    private static boolean works = false;
+    private static Class<?> craftEntityClass = null;
+    private static Method getHandleMethod = null;
+    private static Class<?> nmsEntityClass = null;
+    private static Class<?> nbtTagCompoundClass = null;
+    private static Method setIntMethod = null;
+    private static Method saveNBTTagMethod = null;
+    private static Method loadNBTTagMethod = null;
 
     static {
         try {
-            String nmsver = org.bukkit.Bukkit.getServer().getClass().getPackage()
-                    .getName().split("\\.")[3];
-            clsCraftEntity = Class.forName("org.bukkit.craftbukkit." + nmsver
-                    + ".entity.CraftEntity");
-            mhdGetHandle = clsCraftEntity.getDeclaredMethod("getHandle");
-            Class<?> clsNMSEntity = Class.forName("net.minecraft.server." + nmsver
-                    + ".Entity");
-            mhdGetNBTTag = clsNMSEntity.getDeclaredMethod("getNBTTag");
-            clsNBTTagCompound = Class.forName("net.minecraft.server." + nmsver
-                    + ".NBTTagCompound");
-            mhdSetInt = clsNBTTagCompound.getDeclaredMethod("setInt",
-                    String.class, int.class);
-            mhdReadTag = clsNMSEntity.getDeclaredMethod("c", clsNBTTagCompound);
-            mhdSetTag = clsNMSEntity.getDeclaredMethod("f", clsNBTTagCompound);
+            String nmsver = org.bukkit.Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+            craftEntityClass = Class.forName("org.bukkit.craftbukkit." + nmsver + ".entity.CraftEntity");
+            nmsEntityClass = Class.forName("net.minecraft.server." + nmsver + ".Entity");
+            nbtTagCompoundClass = Class.forName("net.minecraft.server." + nmsver + ".NBTTagCompound");
+
+            getHandleMethod = craftEntityClass.getDeclaredMethod("getHandle");
+
+            // Try to get NBT-related methods for 1.12.2
+            boolean methodsFound = false;
+            try {
+                saveNBTTagMethod = nmsEntityClass.getDeclaredMethod("save", nbtTagCompoundClass);
+                loadNBTTagMethod = nmsEntityClass.getDeclaredMethod("load", nbtTagCompoundClass);
+                setIntMethod = nbtTagCompoundClass.getDeclaredMethod("setInt", String.class, int.class);
+                methodsFound = true;
+            } catch (Exception e1) {
+                // If above methods don't exist, try other possible names in 1.12.2
+                try {
+                    saveNBTTagMethod = nmsEntityClass.getDeclaredMethod("f", nbtTagCompoundClass);
+                    loadNBTTagMethod = nmsEntityClass.getDeclaredMethod("a", nbtTagCompoundClass);
+                    setIntMethod = nbtTagCompoundClass.getDeclaredMethod("a", String.class, int.class);
+                    methodsFound = true;
+                } catch (Exception e2) {
+                    System.out.println("Warning: Could not find required NBT methods for NoAIUtils");
+                }
+            }
+            
+            if (methodsFound) {
+                works = true;
+            }
         } catch (Exception e) {
+            System.out.println("Warning: Failed to initialize NoAIUtils: " + e.getMessage());
             e.printStackTrace();
-            works = false;
         }
-        works = true;
     }
 
     public static void setAI(Entity bukkitEntity, boolean hasAI) {
-        if (!works)
+        if (!works) {
             return;
-        try {
-            Object objCraftEntity = clsCraftEntity.cast(bukkitEntity);
-            Object objNMSEntity = mhdGetHandle.invoke(objCraftEntity);
-            Object objNBTTag = mhdGetNBTTag.invoke(objNMSEntity);
-            if (objNBTTag == null) {
-                objNBTTag = clsNBTTagCompound.getConstructor().newInstance();
-            }
-            mhdReadTag.invoke(objNMSEntity, objNBTTag);
-            mhdSetInt.invoke(objNBTTag, "NoAI", hasAI ? 0 : 1);
-            mhdSetTag.invoke(objNMSEntity, objNBTTag);
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-        /*
-         * net.minecraft.server.v1_8_R3.Entity nmsEntity = ((CraftEntity)
-         * bukkitEntity).getHandle(); NBTTagCompound tag =
-         * nmsEntity.getNBTTag(); if (tag == null) { tag = new NBTTagCompound();
-         * } nmsEntity.c(tag); tag.setInt("NoAI", 1); nmsEntity.f(tag);
-         */
+
+        try {
+            // Get NMS entity
+            Object nmsEntity = getHandleMethod.invoke(bukkitEntity);
+
+            // Create NBT tag
+            Object nbtTag = nbtTagCompoundClass.getConstructor().newInstance();
+
+            // Read entity's NBT data
+            if (saveNBTTagMethod != null) {
+                saveNBTTagMethod.invoke(nmsEntity, nbtTag);
+            }
+
+            // Set NoAI tag
+            if (setIntMethod != null) {
+                setIntMethod.invoke(nbtTag, "NoAI", hasAI ? 0 : 1);
+            }
+
+            // Write modified NBT data back to entity
+            if (loadNBTTagMethod != null) {
+                loadNBTTagMethod.invoke(nmsEntity, nbtTag);
+            }
+        } catch (Exception e) {
+            // Silently fail to avoid spamming server logs
+        }
     }
 }
